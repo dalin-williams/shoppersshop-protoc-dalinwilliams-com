81737607f207409f43f04723decc160733ec5f7d
funkeyfreak <dalinwilliams@yahoo.com>
2018-03-04 20:02:25 -0800

    Modified object responses

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 swagger validate vending/vending.json
 rm -f vending/vending.proto vending.pb.go
 openapi2proto -spec vending/vending.json -options >> vending/vending.proto
-make all
+make all &&
 git add -u
