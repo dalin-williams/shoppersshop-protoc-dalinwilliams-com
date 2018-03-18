40f9edc9e842406b35c63417268c565656ea488b
funkeyfreak <dalinwilliams@yahoo.com>
2018-03-18 08:59:23 -0700

    remove all uuid
    Removed all uuid from every request and object. This allows us to use multiple services, and methods of accessing our endpoints

--- vending/vending.pb.gw.go
+++ vending/vending.pb.gw.go
@@ -115,6 +115,33 @@ func request_SHOPPERSSHOP_VendingMachineService_PayAddToCart_0(ctx context.Conte
 
 }
 
+func request_SHOPPERSSHOP_VendingMachineService_PayDeleteItemFromCart_0(ctx context.Context, marshaler runtime.Marshaler, client SHOPPERSSHOP_VendingMachineServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq PayDeleteItemFromCartRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "id")
+	}
+
+	protoReq.Id, err = runtime.String(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
+	}
+
+	msg, err := client.PayDeleteItemFromCart(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 func request_SHOPPERSSHOP_VendingMachineService_PayUpdateOrder_0(ctx context.Context, marshaler runtime.Marshaler, client SHOPPERSSHOP_VendingMachineServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq PayUpdateOrderRequest
 	var metadata runtime.ServerMetadata
@@ -596,6 +623,35 @@ func RegisterSHOPPERSSHOP_VendingMachineServiceHandlerClient(ctx context.Context
 
 	})
 
+	mux.Handle("DELETE", pattern_SHOPPERSSHOP_VendingMachineService_PayDeleteItemFromCart_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_SHOPPERSSHOP_VendingMachineService_PayDeleteItemFromCart_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_SHOPPERSSHOP_VendingMachineService_PayDeleteItemFromCart_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("PUT", pattern_SHOPPERSSHOP_VendingMachineService_PayUpdateOrder_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -989,6 +1045,8 @@ var (
 
 	pattern_SHOPPERSSHOP_VendingMachineService_PayAddToCart_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"vending", "v1", "pay", "purchase", "id"}, ""))
 
+	pattern_SHOPPERSSHOP_VendingMachineService_PayDeleteItemFromCart_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"vending", "v1", "pay", "purchase", "id"}, ""))
+
 	pattern_SHOPPERSSHOP_VendingMachineService_PayUpdateOrder_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"vending", "v1", "pay", "orderId"}, ""))
 
 	pattern_SHOPPERSSHOP_VendingMachineService_PayReOrderItems_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"vending", "v1", "pay", "orderId"}, ""))
@@ -1028,6 +1086,8 @@ var (
 	forward_SHOPPERSSHOP_VendingMachineService_PayPurchaseCart_0 = runtime.ForwardResponseMessage
 
 	forward_SHOPPERSSHOP_VendingMachineService_PayAddToCart_0 = runtime.ForwardResponseMessage
+
+	forward_SHOPPERSSHOP_VendingMachineService_PayDeleteItemFromCart_0 = runtime.ForwardResponseMessage
 
 	forward_SHOPPERSSHOP_VendingMachineService_PayUpdateOrder_0 = runtime.ForwardResponseMessage
 
